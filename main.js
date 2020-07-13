417cec05b3f15aed02bfcbca041cae57aff123ce
angel-i <50430477+angel-i@users.noreply.github.com>
2020-07-13 17:01:24 -0300

    sigo

---
+++ main.js
@@ -0,0 +1,8 @@
+const iconShare = document.querySelector(".icon");
+const socialBar = document.querySelector(".active-bar");
+
+iconShare.onclick = myFunction;
+
+function myFunction() {
+  iconShare.addEventListener("click", (socialBar.style.opacity = 1));
+}
